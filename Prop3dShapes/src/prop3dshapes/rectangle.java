6d2138e8594c423793d51a644034a130abffe6d5
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-24 09:40:20 -0400

    Creating a subclass of shape2d called rectangle

---
+++ Prop3dShapes/src/prop3dshapes/rectangle.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop3dshapes;
+
+/**
+ *
+ * @author S331474817
+ */
+public class rectangle {
+    public double length;
+    public double width;
+    public double area(double l, double w){
+        return l*w;
+    }
+}
