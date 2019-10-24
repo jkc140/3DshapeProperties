36bb187207225928fd6370a49375fd8c512762ec
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-24 09:29:28 -0400

    creating a class called triangle that is a subclass of shape2d

---
+++ Prop3dShapes/src/prop3dshapes/triangle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop3dshapes;
+
+/**
+ *
+ * @author S331474817
+ */
+public class triangle extends shape2d{
+    public void data(){
+        double sL1;
+        double sL2;
+        double base;
+        double height;
+    }
+    public double area(double b, double h){
+        return b*h/2;
+    }
+}
