56e61fdb435659b058b3f9ffddf12aa72cb3b96e
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-24 09:46:13 -0400

    creating a new package for organizing the different classes creating a new class called shape3d to be an interface containging functions volume and SA

---
+++ Prop3dShapes/src/shape3d/shape3d.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shape3d;
+
+/**
+ *
+ * @author S331474817
+ */
+public class shape3d {
+    public void volume(){
+        System.out.println("The volume is: ");
+    }
+    public void SA(){
+        System.out.println("The Surface Area is: ");
+    }
+}
