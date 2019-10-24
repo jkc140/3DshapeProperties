f4d98787df14d362126afed5f421999231fd703b
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-24 09:28:58 -0400

    creating a new class called shape2d with a function called area.

---
+++ Prop3dShapes/src/prop3dshapes/shape2d.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop3dshapes;
+
+/**
+ *
+ * @author S331474817
+ */
+public class shape2d {
+    public double area(double x){
+        return x;
+    }
+}
