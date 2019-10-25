d99f68cd6a59483729efae51f41f440d563981cb
J C <>
2019-10-24 23:39:02 -0400

    Still WIP added scanner module to get inputs from the user
    begin to shape main function
    - determine what shape is being used
       - could probably create a function or a class to determine the base 
    shape and 3d shape
    - still need to work on this as it is not completed, with the functions 
    to get the area not having the necessary perameters to work

--- Prop3dShapes/src/prop3dshapes/Prop3dShapes.java
+++ Prop3dShapes/src/prop3dshapes/Prop3dShapes.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package prop3dshapes;
-
+import shape3d.*;
+import java.util.Scanner();
 /**
  *
  * @author S331474817
@@ -15,8 +16,37 @@ public class Prop3dShapes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int shape;
+      double BA;
+      bool shLoop=false;
         // TODO code application logic here
         System.out.println("Program Commencing");
+        System.out.println("What is the base shape of the 3d shape?");
+        do{
+          shape=sc.nextInt();
+          shape=shape.toLowerCase();
+          switch(shape){
+            case "triangle":
+
+              triangle.BA();
+              shLoop=true;
+              break
+            case "circle":
+
+              BA=circle.BA(circle.getRad());
+              shLoop=true;
+              break
+            case "rectangle":
+              rectangle.BA();
+              shLoop=true;
+              break
+            default:
+              System.out.println("Not a valid base shape.")
+              System.out.println("Please try again.")
+            }
+
+        }while(shLoop==true);
     }
-    
+
 }
