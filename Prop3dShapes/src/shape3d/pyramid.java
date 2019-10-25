c126e568dfe57eba324aad1fbcd1e119a2a2231b
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-25 11:02:33 -0400

    - removing BA and depth from pyramid as they are parent variables - replacing all BA and depth with their parent variables

--- Prop3dShapes/src/shape3d/pyramid.java
+++ Prop3dShapes/src/shape3d/pyramid.java
@@ -2,22 +2,17 @@ package shape3d;
 import java.util.Scanner;
 
 public class pyramid extends shape3d{
-  public double BA;
-  public double depth;
-  public double v;
   public double sH;
-  public double s;
-
   public void getSidH(){
       Scanner sc = new Scanner(System.in);
       System.out.println("What is the side height of the pyramid");
       sH=sc.nextDouble();
   }
   public double volume(double d){
-    return BA*d/3;
+    return shape3d.BA*shape3d.depth/3;
   }
   public double SA(int sNum, double[] sLen ){
-    return BA+shape3d.sidA(sNum,sLen,sH);
+    return shape3d.BA+shape3d.sidA(sNum,sLen,sH);
   }
 
 }
