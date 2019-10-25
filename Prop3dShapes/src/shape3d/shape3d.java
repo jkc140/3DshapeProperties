c43b848da328a1a931d2e0ca756a594f444e4cde
J C <>
2019-10-24 23:30:38 -0400

    add new variables and functions - variables that are used in all subclass    - depth    - BA add functions to get BA (base area and depth and one for the side area of the shape

--- Prop3dShapes/src/shape3d/shape3d.java
+++ Prop3dShapes/src/shape3d/shape3d.java
@@ -4,16 +4,46 @@
  * and open the template in the editor.
  */
 package shape3d;
-
+import java.util.Scanner();
 /**
  *
  * @author S331474817
  */
 public class shape3d {
+    public depth;
+    public BA;
+    public double sidA(int sNum,double[] sLen){
+      double tempA=0;
+      for(int x=0;x<sNum<x++){
+        tempA=tempA+(sLen[x]*sH);
+      }
+      return tempA();
+    }
+    protected void getDepth(){
+      Scanner sc=new scanner(System.in);
+      System.out.println("What is the height of the 3d shape")
+      depth=sc.nextDouble();
+    }
+    protected void getBA(int shape){
+      switch(shape){
+        case 0:
+          BA=triangle.area();
+          break
+        case 1:
+          BA=circle.area();
+          break
+        case 2:
+          BA=circle.area();
+          break
+        default:
+          System.out.println("Error area not found");
+      }
+    }
+
     public void volume(){
-        System.out.println("The volume is: ");
+      System.out.println("The volume is: ");
     }
     public void SA(){
-        System.out.println("The Surface Area is: ");
+      System.out.println("The Surface Area is: ");
     }
 }
