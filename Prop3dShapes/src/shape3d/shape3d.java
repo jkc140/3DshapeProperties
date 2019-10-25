00edced8ba3f7601e8224bba9c5151f8b0863a8f
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-25 10:40:58 -0400

    making area a static as it was giving errors when calling in prop3dshapes

--- Prop3dShapes/src/shape3d/shape3d.java
+++ Prop3dShapes/src/shape3d/shape3d.java
@@ -4,37 +4,38 @@
  * and open the template in the editor.
  */
 package shape3d;
-import java.util.Scanner();
+import java.util.Scanner;
+import prop3dshapes.*;
 /**
  *
  * @author S331474817
  */
 public class shape3d {
-    public depth;
-    public BA;
-    public double sidA(int sNum,double[] sLen){
+    public double depth;
+    public double BA;
+    public double sidA(int sNum,double[] sLen, double depth){
       double tempA=0;
-      for(int x=0;x<sNum<x++){
-        tempA=tempA+(sLen[x]*sH);
+      for(int x=0;x<sNum;x++){
+        tempA=tempA+(sLen[x]*depth);
       }
-      return tempA();
+      return tempA;
     }
     protected void getDepth(){
-      Scanner sc=new scanner(System.in);
-      System.out.println("What is the height of the 3d shape")
+      Scanner sc=new Scanner(System.in);
+      System.out.println("What is the height of the 3d shape");
       depth=sc.nextDouble();
     }
     protected void getBA(int shape){
       switch(shape){
         case 0:
-          BA=triangle.area();
-          break
+          BA=triangle.area(3,4);
+          break;
         case 1:
-          BA=circle.area();
-          break
+          BA=circle.area(circle.getRad());
+          break;
         case 2:
-          BA=circle.area();
-          break
+          BA=rectangle.area(33,54);
+          break;
         default:
           System.out.println("Error area not found");
       }
