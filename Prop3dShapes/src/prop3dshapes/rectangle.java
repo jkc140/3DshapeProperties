fac415ee7d9009d158c40eac49cfe5a8177d1ef8
J C <>
2019-10-27 12:59:06 -0400

    add functions called getLeng and getWid to get the length an width of the rectangle
    - import scanner to get user input
    - add array called sidLen to hold side length value

--- Prop3dShapes/src/prop3dshapes/rectangle.java
+++ Prop3dShapes/src/prop3dshapes/rectangle.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package prop3dshapes;
-
+import java.util.Scanner;
 /**
  *
  * @author S331474817
@@ -12,6 +12,19 @@ package prop3dshapes;
 public class rectangle extends shape2d{
     public double length;
     public double width;
+    public double[] sidLen =new double[2];
+    public static getLen(){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("What is the length of the rectangle");
+      length=sc.nextDouble();
+      sidLen[0]=length;
+    }
+    public static getWid(){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("What is the width of the rectangle");
+      width=sc.nextDouble();
+      sidLen[1]=width;
+    }
     public static double area(double l, double w){
         return l*w;
     }
