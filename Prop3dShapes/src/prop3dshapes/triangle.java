c4afe6ee281db9e0696aa9da38043dcdeff45229
J C <>
2019-10-27 13:09:07 -0400

    add functions to get the side lengths, base and height of triangle - import scanner module to get user inputs

--- Prop3dShapes/src/prop3dshapes/triangle.java
+++ Prop3dShapes/src/prop3dshapes/triangle.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package prop3dshapes;
-
+import java.util.Scanner;
 /**
  *
  * @author S331474817
@@ -16,6 +16,31 @@ public class triangle extends shape2d{
     public double[] sL=new double[3];
 
     //functions
+    public void getBase(){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("What is the base of the triangle");
+      sL[0]=nextDouble();
+    }
+
+
+    public void getHeight(){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("What is the heigth of the triangle");
+      height=nextDouble();
+
+    }
+
+
+    public void getSid(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("What is the hypotenous of the triangle");
+        sL[1]=nextDouble();
+        System.out.println("What is the other side length of the triangle (Excluding base)");
+        sL[2]=nextDouble();
+
+    }
+
+
     public static double area(double b, double h){
         return b*h/2;
     }
