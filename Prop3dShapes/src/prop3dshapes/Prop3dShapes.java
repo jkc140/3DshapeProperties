62134e61625d48d1a6055c14c7476fa3cb363802
J C <>
2019-10-27 23:19:43 -0400

    making code more efficient - changing all the sort functions into 2 functions  - getBaseShape()  - get3dShape - both do the samething as the sort functions, but instead it creates an instance of the subclass corresponding to the shape and it sets the user2dshape or user3dshape to the corresponding ones. - actually use instanstation

--- Prop3dShapes/src/prop3dshapes/Prop3dShapes.java
+++ Prop3dShapes/src/prop3dshapes/Prop3dShapes.java
@@ -12,108 +12,71 @@ import java.util.Scanner;
  * @author S331474817
  */
 public class Prop3dShapes {
-    public static String sortShap(){
-        Scanner sc=new Scanner(System.in);
-        String shape;
-        boolean shLoop=false; //<-- shape loop (sh + loop)
+    public shape2d getBaseShape(){
+      Scanner sc=new Scanner(System.in);
+      shape2d inst2dShape=null;
+      boolean shapeCheck =false;
+      do{
         System.out.println("What is the base shape of the 3d shape? (triangle, rectangle or square)");
-        do{
-          shape=sc.nextLine();
-          switch(shape.toLowerCase()){
-            case "triangle":
-              shLoop=true;
-              break;
-            case "circle":
-              shLoop=true;
-              break;
-            case "rectangle":
-              shLoop=true;
-              break;
-            default:
-              System.out.println("Not a valid base shape.");
-              System.out.println("Please try again.");
-            }
+        shape=sc.nextLine();
+        switch(shape.toLowerCase()){
+          case "triangle":
+            inst2dShape=new triangle()
+            shLoop=true;
+            break;
+          case "circle":
+            inst2dShape=new circle()
+            shLoop=true;
+            break;
+          case "rectangle":
+            inst2dShape=new rectangle()
+            shLoop=true;
+            break;
+          default:
+            System.out.println("Not a valid base shape.");
+            System.out.println("Please try again.");
+          }
 
-        }while(shLoop==false);
-        return shape.toLowerCase();
+      }while(shLoop==false);
+      inst2dShape.getProps();
+      return inst2dShape;
     }
-    public static double sortBA(String shape){
-        triangle trig = new triangle();
-        circle circ=new circle();
-        rectangle rect=new rectangle();
-        double BA=0;
-        switch(shape){
-            case "triangle":
-                BA= trig.area(trig.getBase(),trig.getHeight());
-                break;
-            case "circle":
-                BA= circ.area(circ.getRad());
-                break;
-            case "rectangle":
-                 BA= rect.area(rect.getLen(),rect.getWid());
-                 break;
-            default:
-                System.out.println("Error Error Error Cannot Compute Error Error Error");
-        }
-        return BA;
-       }
+    public shape3d get3dShape(shape2d inst2dShape){
+      Scanner sc=new Scanner(System.in);
+      shape3d inst3dShape=null;
+      boolean shapeCheck =false;
+      do{
+        System.out.println("What type of 3d shape are you entering (prism or pyramid)");
+        shape=sc.nextLine();
+        switch(shape.toLowerCase()){
+          case "triangle":
+            inst3dShape=new Prism()
+            shLoop=true;
+            break;
+          case "circle":
+            inst3dShape=new pyramid()
+            shLoop=true;
+            break;
+          default:
+            System.out.println("Not a valid base shape.");
+            System.out.println("Please try again.");
+          }
+      }while(shLoop==false);
+      inst3dShape.setBase(inst2dShape.area());
+      inst3dShape.getDepth();
+      inst3dShape.sidA(inst2dShape);
+      return inst3dShape;
+    }
 
-       public static void sort3d(double d,double BA,int sNum,double[] sLen){
-         String shape;
-         System.out.println("What type of 3d shape are you entering (prism or pyramid)");
-         Scanner sc=new Scanner(System.in);
-         boolean shLoop=false; //<-- shape loop (sh + loop)
-         do{
-          shape=sc.nextLine();
-          switch(shape.toLowerCase()){
-            case "pyramid":
-                pyramid.getSidH();
-                System.out.print("You're shape has a volume of ");
-                System.out.println(pyramid.volume(BA,d));
-                System.out.print("You're shape has a volume of ");
-                System.out.println(pyramid.SA(BA,sNum,sLen));
-                shLoop=true;
-                break;
-              case "prism":
-                System.out.print("You're shape has a volume of ");
-                System.out.println(Prism.volume(d,BA));
-                System.out.print("You're shape has a volume of ");
-                System.out.println(Prism.SA(d,BA,sNum,sLen));
-                shLoop=true;
-                break;
-              default:
-                System.out.println("Error Invalid 3d shape");
-                System.out.println("Please try again");
-            }
-         }while(shLoop==false);
-       }
-       public static void sortBash(String shape,double ba){
-         switch(shape){
-             case "triangle":
-                 sort3d(shape3d.getDepth(),ba,3,triangle.sL);
-                 break;
-             case "circle":
-                 circle.circ();
-                 sort3d(shape3d.getDepth(),ba,1,circle.circumf);
-                 break;
-             case "rectangle":
-                 sort3d(shape3d.getDepth(),ba,4,rectangle.sidLen);
-                 break;
-             default:
-                 System.out.println("Error Error Error Cannot Compute Error Error Error");
-         }
-       }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
       System.out.println("Program Commencing");
-
-        String bash=sortShap(); //<-- base shape (ba+ sh)
-        sortBash(bash,shape3d.getBA(sortBA(bash)));
-
-
-
-    }
+      shape2d user2dShape=get2dshape();
+      shape3d user3dShape=get3dShape();
+      System.out.println("The volume of your shape is :" + user3dShape.volume());
+      System.out.println("The Surface Area of your shape is :" + user3dShape.SA());
+      }
 
 }
