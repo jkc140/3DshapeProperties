8bb4bb6c6c650d6ec7f1d1d1422368cbc8862fae
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2019-10-28 09:00:46 -0400

    - finishing commiting

--- Prop3dShapes/src/prop3dshapes/shape2d.java
+++ Prop3dShapes/src/prop3dshapes/shape2d.java
@@ -9,12 +9,12 @@ package prop3dshapes;
  *
  * @author S331474817
  */
-public class shape2d {
-    public double baseArea;
-    public double[] sideLen;
-    public double area() {
+public class shape2d { //create a class called shape2d
+    public double baseArea; //creaate a variable to hold the base area
+    public double[] sideLen; //create an array called sideLen to hold all the side lengths
+    public double area() { //creating a function called area to get the area of the function
     	return 0;
     }
-    public void getProps() {
+    public void getProps() { //create a function called getProps to get the properties of the 2d shape
     	}
     }
